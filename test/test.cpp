83f9f2191ebe277b6094379c9dd9d3c9577acdad
Alexander Fabisch <afabisch@googlemail.com>
2012-04-30 17:08:26 +0200

    Add test suite source file.

---
+++ test/test.cpp
@@ -0,0 +1,46 @@
+#include "Test/TestSuite.h"
+#include "Test/TextTestRunner.h"
+#ifdef USE_QT
+#include "Test/QtTestRunner.h"
+#endif
+#include <iostream>
+#include <cstdlib>
+
+//#include <io/Logger.h>
+
+int main(int argc, char** argv)
+{
+  bool verbose = false;
+  bool qt = false;
+  for(int i = 1; i < argc; i++)
+  {
+    std::string argument(argv[i]);
+    if(argument == std::string("-v"))
+      verbose = true;
+    else if(argument == std::string("-qt"))
+      qt = true;
+  }
+
+  //Logger::deactivate = true;
+
+  TestSuite ts("OpenANN");
+  //ts.addTestCase(new ...TestCase); // TODO add test cases
+
+  if(qt)
+  {
+#ifdef USE_QT
+    QtTestRunner qtr(argc, argv);
+    qtr.run(ts);
+#else
+    std::cerr << "Qt is not available." << std::endl;
+    return EXIT_FAILURE;
+#endif
+  }
+  else
+  {
+    TextTestRunner ttr(verbose);
+    ttr.run(ts);
+  }
+
+  return EXIT_SUCCESS;
+}
