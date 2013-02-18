c35f492336110dc9acb7299fd08045f38ce138df
Alexander Fabisch <afabisch@googlemail.com>
2013-02-18 19:27:36 +0100

    Fix compiler error.

--- test/PreprocessingTestCase.cpp
+++ test/PreprocessingTestCase.cpp
@@ -10,15 +10,15 @@ void PreprocessingTestCase::run()
 void PreprocessingTestCase::scaling()
 {
   Mt data = Mt::Random(20, 20);
-  OpenANN::scaleData(data, 5.0, 10.0);
-  ASSERT_EQUALS_DELTA(data.minCoeff(), 5.0, 1e-3);
-  ASSERT_EQUALS_DELTA(data.maxCoeff(), 10.0, 1e-3);
-  OpenANN::scaleData(data, -5.0, 5.0);
-  ASSERT_EQUALS_DELTA(data.minCoeff(), -5.0, 1e-3);
-  ASSERT_EQUALS_DELTA(data.maxCoeff(), 5.0, 1e-3);
+  OpenANN::scaleData(data, (fpt) 5.0, (fpt) 10.0);
+  ASSERT_EQUALS_DELTA(data.minCoeff(), (fpt) 5.0, (fpt) 1e-3);
+  ASSERT_EQUALS_DELTA(data.maxCoeff(), (fpt) 10.0, (fpt) 1e-3);
+  OpenANN::scaleData(data, (fpt) -5.0, (fpt) 5.0);
+  ASSERT_EQUALS_DELTA(data.minCoeff(), (fpt) -5.0, (fpt) 1e-3);
+  ASSERT_EQUALS_DELTA(data.maxCoeff(), (fpt) 5.0, (fpt) 1e-3);
   try
   {
-    OpenANN::scaleData(data, 1.0, -1.0);
+    OpenANN::scaleData(data, (fpt) 1.0, (fpt) -1.0);
     ASSERT(false);
   }
   catch(OpenANN::OpenANNException& e)
