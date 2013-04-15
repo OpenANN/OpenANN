3aa7df44fbb3407bca5af8b17eb9928dc0afb508
Alexander Fabisch <afabisch@googlemail.com>
2013-04-15 21:31:50 +0200

    Refactor and test on MNIST.

--- src/StoppingInterrupt.cpp
+++ src/StoppingInterrupt.cpp
@@ -1,47 +1,46 @@
 #include <OpenANN/optimization/StoppingInterrupt.h>
 #include <OpenANN/util/AssertionMacros.h>
-
 #include <csignal>
 
 namespace OpenANN {
 
 
-bool StoppingInterrupt::stopping_interrupt_signal = false;
+bool StoppingInterrupt::stoppingInterruptSignal = false;
 int  StoppingInterrupt::observers = 0;
 
 
 StoppingInterrupt::StoppingInterrupt()
 {
-    OPENANN_CHECK(!stopping_interrupt_signal);
+  OPENANN_CHECK(!stoppingInterruptSignal);
 
-    if(observers == 0) 
-        std::signal(SIGINT, StoppingInterrupt::setStoppingInterruptSignal);
+  if(observers == 0) 
+    std::signal(SIGINT, StoppingInterrupt::setStoppingInterruptSignal);
 
-    ++observers;
+  ++observers;
 }
 
 StoppingInterrupt::~StoppingInterrupt()
 {
-    --observers;
+  --observers;
 
-    if(observers == 0) {
-        std::signal(SIGINT, SIG_DFL);
-        stopping_interrupt_signal = false;
-    }
+  if(observers == 0)
+  {
+    std::signal(SIGINT, SIG_DFL);
+    stoppingInterruptSignal = false;
+  }
 }
 
 
-void StoppingInterrupt::setStoppingInterruptSignal(int param) 
+void StoppingInterrupt::setStoppingInterruptSignal(int param)
 {
-    stopping_interrupt_signal = true;
+  stoppingInterruptSignal = true;
 }
 
 
 bool StoppingInterrupt::isSignaled()
 {
-    OPENANN_CHECK(observers > 0);
-
-    return stopping_interrupt_signal;
+  OPENANN_CHECK(observers > 0);
+  return stoppingInterruptSignal;
 }
 
 }
