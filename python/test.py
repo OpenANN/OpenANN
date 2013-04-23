6ff0543752e17fbc7de087dcf8a132a576fb95d2
Alexander Fabisch <Alexander.Fabisch@dfki.de>
2013-04-23 18:37:00 +0200

    Simplify test script a little bit.

--- python/test.py
+++ python/test.py
@@ -3,7 +3,7 @@ from numpy import *
 import pylab
 
 stop  = {
-    "maximal_iterations": 20,
+    "maximal_iterations": 50,
     "minimal_value_differences": 1e-8
 }
 
@@ -14,14 +14,13 @@ net.input_layer(1, 1)
 net.fully_connected_layer(200, Activation.LOGISTIC)
 net.output_layer(1, Activation.LINEAR)
 
-inputs = numpy.atleast_2d(numpy.linspace(0, 2 * numpy.pi, 500))
-outputs = numpy.sin(numpy.random.normal(inputs, numpy.ones(1) * 0.1))
+inputs = numpy.atleast_2d(numpy.linspace(0, 2*numpy.pi, 500))
+outputs = numpy.sin(numpy.random.normal(inputs, 0.1))
 
 lma.optimize(net, inputs, outputs)
 
-prediction = []
-for i in range(inputs.shape[1]):
-  prediction.append(net.predict(inputs[:, i])[0])
+prediction = [net.predict(inputs[:, i])[0]
+              for i in range(inputs.shape[1])]
 
 pylab.plot(inputs[0], outputs[0], ".", label="Data Set")
 pylab.plot(inputs[0], prediction, label="Prediction")
