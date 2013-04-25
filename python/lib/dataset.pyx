a793ef0e040338a3f3f23f020c26f35a04e573d9
Chris Mueller <christoph.mueller@dfki.de>
2013-04-25 16:27:31 +0200

    python: dataset can be loaded now from libsvm parser.

--- python/lib/dataset.pyx
+++ python/lib/dataset.pyx
@@ -1,53 +1,53 @@
 cdef class Dataset:
   cdef openann.MatrixXd* input
   cdef openann.MatrixXd* output
-  cdef openann.DirectStorageDataSet* dataset
+  cdef openann.DataSet* storage
 
   def __cinit__(self, input, output):
     self.input = __matrix_numpy_to_eigen__(input)
     self.output = __matrix_numpy_to_eigen__(output)
-    self.dataset = new openann.DirectStorageDataSet(deref(self.input), deref(self.output), openann.NONE, 0)
+    self.storage = new openann.DirectStorageDataSet(deref(self.input), deref(self.output))
 
   def __cinit__(self):
     self.input = NULL
     self.output = NULL
-    self.dataset = NULL
+    self.storage = NULL
 
   def __dealloc__(self):
-    if(self.dataset != NULL):
-      del self.dataset
+    if(self.storage != NULL):
+      del self.storage
     if(self.input != NULL):
       del self.input
     if(self.output != NULL):
       del self.output
 
   def samples(self):
-    if self.dataset != NULL:
-      return self.dataset.samples()
+    if self.storage != NULL:
+      return self.storage.samples()
     else:
       return 0
 
   def inputs(self):
-    if self.dataset != NULL:
-      return self.dataset.inputs()
+    if self.storage != NULL:
+      return self.storage.inputs()
     else:
       return 0
 
   def outputs(self):
-    if self.dataset != NULL:
-      return self.dataset.outputs()
+    if self.storage != NULL:
+      return self.storage.outputs()
     else:
       return 0
 
   def instance(self, i):
-    if self.dataset != NULL:
-      return __vector_eigen_to_numpy__(&self.dataset.getInstance(i))
+    if self.storage != NULL:
+      return __vector_eigen_to_numpy__(&self.storage.getInstance(i))
     else:
       return 0
 
   def target(self, i):
-    if self.dataset != NULL:
-      return __vector_eigen_to_numpy__(&self.dataset.getTarget(i))
+    if self.storage != NULL:
+      return __vector_eigen_to_numpy__(&self.storage.getTarget(i))
     else:
       return 0
 
@@ -58,7 +58,7 @@ def load_from_libsvm(filename):
   dset = Dataset()
   dset.input = input
   dset.output = output
-  openann.load_from_libsvm(deref(input), deref(output), filename, 0)
-  dset.dataset = new openann.DirectStorageDataSet(deref(input), deref(output), openann.NONE, 0)
+  openann.libsvm_load(deref(input), deref(output), filename, 0)
+  dset.storage = new openann.DirectStorageDataSet(deref(input), deref(output))
   return dset 
 
