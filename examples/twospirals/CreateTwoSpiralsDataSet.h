a41e6eb0c05610d7c03d415e2099c00257e96828
Alexander Fabisch <afabisch@googlemail.com>
2013-05-06 22:39:38 +0200

    Fix examples.

--- examples/twospirals/CreateTwoSpiralsDataSet.h
+++ examples/twospirals/CreateTwoSpiralsDataSet.h
@@ -2,6 +2,7 @@
 
 #include <Eigen/Dense>
 #include <OpenANN/util/AssertionMacros.h>
+#include <OpenANN/Preprocessing.h>
 
 /**
  * Creates two interlocked spirals that form different classes.
@@ -17,15 +18,17 @@
  * @param Xte test inputs
  * @param Yte test outputs
  */
-void createTwoSpiralsDataSet(int density, double maxDiameter, Eigen::MatrixXd& Xtr, Eigen::MatrixXd& Ytr, Eigen::MatrixXd& Xte, Eigen::MatrixXd& Yte)
+void createTwoSpiralsDataSet(int density, double maxDiameter,
+                             Eigen::MatrixXd& Xtr, Eigen::MatrixXd& Ytr,
+                             Eigen::MatrixXd& Xte, Eigen::MatrixXd& Yte)
 {
   // Number of interior data points per spiral to generate
   const int points = 96 * density;
 
-  Xtr.resize(2, points+1);
-  Ytr.resize(1, points+1);
-  Xte.resize(2, points+1);
-  Yte.resize(1, points+1);
+  Xtr.resize(points+1, 2);
+  Ytr.resize(points+1, 1);
+  Xte.resize(points+1, 2);
+  Yte.resize(points+1, 1);
   int trIdx = 0;
   int teIdx = 0;
 
@@ -41,41 +44,31 @@ void createTwoSpiralsDataSet(int density, double maxDiameter, Eigen::MatrixXd& X
 
     if(i == points)
     {
-      Xtr(0, trIdx) = x;
-      Xtr(1, trIdx) = y;
-      Ytr(0, trIdx) = 1.0;
-      Xte(0, trIdx) = -x;
-      Xte(1, teIdx) = -y;
-      Yte(0, teIdx) = -1.0;
+      Xtr.row(trIdx) << x, y;
+      Ytr.row(trIdx) << 1.0;
+      Xte.row(trIdx) << -x, -y;
+      Yte.row(teIdx) << -1.0;
     }
     else if(i % 2 == 0)
     {
       OPENANN_CHECK_WITHIN(trIdx, 0, points);
-      Xtr(0, trIdx) = x;
-      Xtr(1, trIdx) = y;
-      Ytr(0, trIdx) = 1.0;
-      Xtr(0, trIdx+1) = -x;
-      Xtr(1, trIdx+1) = -y;
-      Ytr(0, trIdx+1) = -1.0;
+      Xtr.row(trIdx) << x, y;
+      Ytr.row(trIdx) << 1.0;
+      Xtr.row(trIdx+1) << -x, -y;
+      Ytr.row(trIdx+1) << -1.0;
       trIdx += 2;
     }
     else
     {
       OPENANN_CHECK_WITHIN(teIdx, 0, points);
-      Xte(0, teIdx) = x;
-      Xte(1, teIdx) = y;
-      Yte(0, teIdx) = 1.0;
-      Xte(0, teIdx+1) = -x;
-      Xte(1, teIdx+1) = -y;
-      Yte(0, teIdx+1) = -1.0;
+      Xte.row(teIdx) << x, y;
+      Yte.row(teIdx) << 1.0;
+      Xte.row(teIdx+1) << -x, -y;
+      Yte.row(teIdx+1) << -1.0;
       teIdx += 2;
     }
   }
 
-  Eigen::MatrixXd shift = Eigen::MatrixXd::Ones(Xtr.rows(), Xtr.cols()) * 0.5;
-  // Scaling
-  Xtr /= 2.0;
-  Xtr += shift;
-  Xte /= 2.0;
-  Xte += shift;
+  OpenANN::scaleData(Xte, 0.0, 1.0);
+  OpenANN::scaleData(Xtr, 0.0, 1.0);
 }
