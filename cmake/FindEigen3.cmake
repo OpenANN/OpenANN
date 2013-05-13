2a37e81251985ab31f96433d1e42961ad727c304
Alexander Fabisch <afabisch@googlemail.com>
2013-05-13 21:53:46 +0200

    Add library path for MacOS.

--- cmake/FindEigen3.cmake
+++ cmake/FindEigen3.cmake
@@ -1,6 +1,7 @@
 find_path(EIGEN3_INCLUDE_DIRS Eigen/Dense
   ${CMAKE_INSTALL_PREFIX}/include/eigen3
   /usr/include/eigen3
+  /opt/local/include/eigen3
   DOC "Eigen 3 include directory")
 
 set(EIGEN3_FOUND ${EIGEN3_INCLUDE_DIRS} CACHE BOOL "" FORCE)
