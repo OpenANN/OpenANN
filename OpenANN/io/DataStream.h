10c2877243f663e67db44cf64a274c4fd4d6f325
Alexander Fabisch <afabisch@tzi.de>
2013-06-02 16:04:59 +0200

    Document DataStream.

--- OpenANN/io/DataStream.h
+++ OpenANN/io/DataStream.h
@@ -10,6 +10,18 @@ class DirectStorageDataSet;
 class Optimizer;
 class Learner;
 
+/**
+ * @class DataStream
+ *
+ * Streams training data for online training.
+ *
+ * A DataStream combines a learner and an optimizer. It is able to cache
+ * training data and will start an optimization epoch if the cache is full.
+ * Training instances will be added incrementally with
+ * DataStream::addSample(). The optimization algorithm that will be passed
+ * with DataStream::setOptimizer() should work for online learning like e.g.
+ * MBSGD.
+ */
 class DataStream
 {
   int cacheSize, collected;
@@ -18,10 +30,28 @@ class DataStream
   Optimizer* opt; //!< Do not delete this!
   Learner* learner; //!< Do not delete this!
 public:
+  /**
+   * Create a data stream.
+   * @param cacheSize size of the internal data cache
+   */
   DataStream(int cacheSize);
   ~DataStream();
+  /**
+   * Add a learner.
+   * @param learner model
+   */
   void setLearner(Learner& learner);
+  /**
+   * Add an optimizer.
+   * @param opt online optimization algorithm
+   */
   void setOptimizer(Optimizer& opt);
+  /**
+   * Add a sample of the data distribution. This will start an optimization
+   * epoch if the cache is full.
+   * @param x input
+   * @param t target (can be 0 for unsupervised learning)
+   */
   void addSample(Eigen::VectorXd* x, Eigen::VectorXd* t = 0);
 private:
   void initialize(int inputs, int outputs);
