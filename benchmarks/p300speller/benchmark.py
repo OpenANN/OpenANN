b11df6a2186741b13eadc6f7a84ed31a639032fa
Alexander Fabisch <afabisch@tzi.de>
2013-06-08 16:50:00 +0200

    Write target chars.

--- benchmarks/p300speller/benchmark.py
+++ benchmarks/p300speller/benchmark.py
@@ -2,7 +2,8 @@ import os
 import sys
 import urllib
 
-LABEL_FILES = ["true_labels_%s.txt" % subject for subject in ["a", "b"]]
+label_file_template = "true_labels_%s.txt"
+LABEL_FILES = [label_file_template % subject for subject in ["a", "b"]]
 LABEL_URLS = ["http://www.bbci.de/competition/iii/results/albany/%s" % f
               for f in LABEL_FILES]
 
@@ -21,24 +22,34 @@ def print_usage():
 
 
 def download_p300speller(directory):
-    if not os.path.exists(directory + "/" + "Subject_A_Train_Flashing.txt"):
+    if not os.path.exists(directory + os.sep + "Subject_A_Train_Flashing.txt"):
         print("Please register at http://www.bbci.de/competition/iii\n"
             "to download the data set II from the BCI competition III.")
 
-    if not os.path.exists(directory + "/" + LABEL_FILES[0]):
+    if not os.path.exists(directory + os.sep + LABEL_FILES[0]):
         for i in range(len(LABEL_URLS)):
           print("Downloading %s" % LABEL_URLS[i])
           downloader = urllib.urlopen(LABEL_URLS[i])
-          open(directory + "/" + LABEL_FILES[i], "w").write(downloader.read())
+          open(directory + os.sep + LABEL_FILES[i], "w").write(downloader.read())
 
-    if True:#not os.path.exists(directory + "/" + "Subject_A_Test_StimulusType.txt"):
+    if True:#not os.path.exists(directory + os.sep + "Subject_A_Test_StimulusType.txt"):
         convert_test_targets()
 
 def convert_test_targets():
     print("Converting test targets...")
-    for f in [directory + "/" + f for f in LABEL_FILES]:
-      print(f)
+    for subject in ["a", "b"]:
+      f = directory + os.sep + label_file_template % subject
 
+      # Write Subject_A/B_Test_TargetChar.txt
+      target_chars = open(f, "r").read().strip()
+      target_ascii = [str(ord(c)) for c in target_chars]
+      f = "dummy.txt" #directory + os.sep + "Subject_%s_Test_TargetChar.txt" % subject.capitalize()
+      open(f, "w").write("\n".join(target_ascii))
+
+      # Write Subject_A/B_Test_StimulusType.txt
+      f = "dummy2.txt" #directory + os.sep + "Subject_%s_Test_StimulusType.txt" % subject.capitalize()
+      stimulus_type = open(directory + os.sep + "Subject_%s_Test_StimulusCode.txt" % subject.capitalize(), "r")
+      # TODO
 
 def run_p300speller(directory):
     pass
