6932e8037ca28095a20f7b676abdf19692c1383e
Alexander Fabisch <afabisch@googlemail.com>
2013-06-17 22:06:35 +0200

    Clean up examples.

--- python/iris.py
+++ python/iris.py
@@ -1,43 +1,46 @@
 from sklearn import datasets
 from openann import *
 
+# Load IRIS dataset
 iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
 D = X.shape[1]
-F = 3
+F = len(numpy.unique(Y))
 N = len(X)
 
+# Preprocess data (normalization and 1-of-c encoding)
 X = (X - X.mean(axis=0)) / X.std(axis=0)
 T = numpy.zeros((N, F))
-for n in range(N):
-    T[n, Y[n]] = 1.0
+T[(range(N), Y)] = 1.0
 
 net = Net()
-stop  = {
-    "maximal_iterations": 1000
-}
-optimizer = MBSGD(
-    stop,
-    learning_rate=0.7, learning_rate_decay=0.99897, min_learning_rate=0.00001,
-    momentum=0.5,
-    batch_size=16)
-
-net.set_regularization(0.001, 0.0, 0.0)
+net.set_regularization(0.0, 0.01, 0.0)
 net.input_layer(D)
-net.fully_connected_layer(200, Activation.RECTIFIER)
-net.fully_connected_layer(200, Activation.RECTIFIER)
+net.fully_connected_layer(100, Activation.RECTIFIER)
+net.fully_connected_layer(100, Activation.RECTIFIER)
 net.output_layer(F, Activation.LINEAR)
 net.set_error_function(Error.CE)
 
+# Split dataset into training set and validation set and make sure that
+# each class is equally distributed in the datasets
 X1 = numpy.vstack((X[0:40], X[50:90], X[100:140]))
 T1 = numpy.vstack((T[0:40], T[50:90], T[100:140]))
+training_set = Dataset(X1, T1)
 X2 = numpy.vstack((X[40:50], X[90:100], X[140:150]))
 T2 = numpy.vstack((T[40:50], T[90:100], T[140:150]))
-training_set = Dataset(X1, T1)
-test_set = Dataset(X2, T2)
+validation_set = Dataset(X2, T2)
 
+# Train for 500 episodes (with tuned parameters for MBSGD)
+optimizer = MBSGD({"maximal_iterations": 500}, learning_rate=0.7,
+    learning_rate_decay=0.999, min_learning_rate=0.001, momentum=0.5,
+    batch_size=16)
 optimizer.optimize(net, training_set)
 
-print classification_hits(net, training_set)
-print classification_hits(net, test_set)
+print("")
+print("Iris data set has %d inputs, %d classes and %d examples" % (D, F, N))
+print("The data has been split up input training and validation set.")
+print("Correct predictions on training set: %d/%d"
+      % (classification_hits(net, training_set), len(X1)))
+print("Correct predictions on test set: %d/%d"
+      % (classification_hits(net, validation_set), len(X2)))
--- python/test.py
+++ python/test.py
@@ -2,34 +2,36 @@ from openann import *
 import numpy
 import pylab
 
-stop  = {
-    "maximal_iterations": 50,
-    "minimal_value_differences": 1e-8
-}
-
+# Create network
 net = Net()
-lma = LMA(stop)
-
-net.input_layer(1)
 net.set_regularization(0.0, 0.0001, 0.0)
+net.input_layer(1)
 net.fully_connected_layer(10, Activation.LOGISTIC)
 net.fully_connected_layer(10, Activation.LOGISTIC)
 net.output_layer(1, Activation.LINEAR)
 
-inputs = numpy.linspace(0, 2*numpy.pi, 500)[:, numpy.newaxis]
-outputs = numpy.sin(inputs) + numpy.random.randn(*inputs.shape) * 0.1
+# Create dataset
+X = numpy.linspace(0, 2*numpy.pi, 500)[:, numpy.newaxis]
+T = numpy.sin(X) + numpy.random.randn(*X.shape) * 0.1
+dataset = Dataset(X, T)
 
-dataset = Dataset(inputs, outputs)
+Log.info("Using %d samples with %d inputs and %d outputs"
+    % (dataset.samples(), dataset.inputs(), dataset.outputs()))
 
-Log.info("Using {0} samples with {1} inputs and {2} outputs".
-    format(dataset.samples(), dataset.inputs(), dataset.outputs()))
-
+# Train network
+stop  = {
+    "maximal_iterations": 50,
+    "minimal_value_differences": 1e-8
+}
+lma = LMA(stop)
 lma.optimize(net, dataset)
 
-prediction = net.predict(inputs)
+# Predict data
+Y = net.predict(X)
 
-pylab.plot(inputs[:, 0], outputs[:, 0], ".", label="Data Set")
-pylab.plot(inputs[:, 0], prediction[:, 0], label="Prediction")
+# Plot dataset and hypothesis
+pylab.plot(X, T, ".", label="Data Set")
+pylab.plot(X, Y, label="Prediction", linewidth=3)
 pylab.legend()
 pylab.show()
 
