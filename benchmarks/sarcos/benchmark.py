760317debf1d589b432eb7dde03c9b0b721365c3
Alexander Fabisch <afabisch@tzi.de>
2013-06-19 20:44:44 +0200

    Comment and optimize SARCOS benchmark.

--- benchmarks/sarcos/benchmark.py
+++ benchmarks/sarcos/benchmark.py
@@ -1,11 +1,50 @@
+## \page SARCOS SARCOS Inverse Dynamics Problem
+#
+#  The SARCOS dataset is taken from
+#  <a href="http://www.gaussianprocess.org/gpml/data/" target=_blank>this</a>
+#  website. This is an inverse dynamics problem, i.e. we have to predict the
+#  7 joint torques given the joint positions, velocities and accelerations.
+#  Hence, we have to solve a regression problem with 21 inputs and 7 outputs
+#  and a very nonlinear function.
+#
+#  The optimization problem is very hard. Underfitting is a much bigger
+#  problem than overfitting. For this reason, we need a very big network
+#  that has four hidden layers with 200 nodes each. The deep architecture
+#  makes the optimization problem very hard but it is more efficient than a
+#  shallow network. However, we can do two things to increase the optimization
+#  speed drastically: we use a non-saturating activation function (rectified
+#  linear units) and mini-batch stochastic gradient descent.
+#
+#  You can start the benchmark with the script:
+#  \verbatim
+#  python benchmark.py [download] [run]
+#  \endverbatim
+#  Note that you need SciPy to load the dataset and matplotlib to display some
+#  results.
+#
+#  The output will look like
+#  \verbatim
+#  Dimension 1: nMSE = 0.938668% (training) / 0.903342% (validation)
+#  Dimension 2: nMSE = 0.679012% (training) / 0.647091% (validation)
+#  Dimension 3: nMSE = 0.453497% (training) / 0.442720% (validation)
+#  Dimension 4: nMSE = 0.242476% (training) / 0.240360% (validation)
+#  Dimension 5: nMSE = 1.010049% (training) / 1.044068% (validation)
+#  Dimension 6: nMSE = 0.851110% (training) / 0.796895% (validation)
+#  Dimension 7: nMSE = 0.474232% (training) / 0.465929% (validation)
+#  \endverbatim
+#  You see the normalized mean squared error (nMSE) for each output dimension
+#  on the training set and the test set. The nMSE is the mean squared error
+#  divided by the variance of the corresponding output dimension. In addition,
+#  a plot that compares the actual and the predicted output of one dimension
+#  will occur.
+
 import os
 import sys
 import urllib
 try:
-    import pylab
     import scipy.io
 except:
-    print("SciPy and matplotlib are required for this benchmark.")
+    print("SciPy is required for this benchmark.")
     exit(1)
 try:
     from openann import *
@@ -58,9 +97,6 @@ def run_sarcos():
     Ystd = Y.std(axis=0)
     Y = (Y - Ymu) / Ystd
     Ytest = (Ytest - Ymu) / Ystd
-    # Learn only one dimension
-    #Y = Y[:, 0, numpy.newaxis]
-    #Ytest = Ytest[:, 0, numpy.newaxis]
 
     training_set = Dataset(X, Y)
     validation_set = Dataset(Xtest, Ytest)
@@ -69,18 +105,16 @@ def run_sarcos():
     F = Y.shape[1]
     net = Net()
     net.input_layer(D)
-    net.fully_connected_layer(250, Activation.TANH)
-    net.fully_connected_layer(100, Activation.TANH)
+    net.fully_connected_layer(200, Activation.RECTIFIER)
+    net.fully_connected_layer(200, Activation.RECTIFIER)
+    net.fully_connected_layer(200, Activation.RECTIFIER)
+    net.fully_connected_layer(200, Activation.RECTIFIER)
     net.output_layer(F, Activation.LINEAR)
 
     stop_dict = {"maximal_iterations" : 100}
     opt = MBSGD(stop_dict, learning_rate=0.2, learning_rate_decay=0.9999,
                 min_learning_rate=0.001, momentum=0.5, batch_size=128)
     opt.optimize(net, training_set)
-
-    print("RMSE")
-    print("Training:", rmse(net, training_set))
-    print("Validation:", rmse(net, validation_set))
 
     pred = net.predict(X)
     pred_test = net.predict(Xtest)
@@ -89,7 +123,13 @@ def run_sarcos():
         nMSE_train = sum((Y[:, f] - pred[:, f])**2) / len(Y) / var[f]
         nMSE_test = sum((Ytest[:, f] - pred_test[:, f])**2) / len(Ytest) / var[f]
         print("Dimension %d: nMSE = %f%% (training) / %f%% (validation)"
-              % (f, nMSE_train*100, nMSE_test*100))
+              % (f+1, nMSE_train*100, nMSE_test*100))
+
+    try:
+        import pylab
+    except:
+        print("Cannot plot the result. Matplotlib is not available.")
+        exit(1)
 
     dim = 0
     n_samples = 200
