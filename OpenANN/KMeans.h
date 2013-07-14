3fd6f477f19c3395d0f4f82d1ebcbeab00c4942a
Alexander Fabisch <afabisch@tzi.de>
2013-07-14 14:42:39 +0200

    Separate definition and implementation.

--- OpenANN/KMeans.h
+++ OpenANN/KMeans.h
@@ -3,7 +3,6 @@
 
 #include <OpenANN/util/Random.h>
 #include <Eigen/Dense>
-#include <limits>
 #include <vector>
 
 namespace OpenANN
@@ -19,89 +18,37 @@ class KMeans
   RandomNumberGenerator rng;
   std::vector<int> clusterIndices;
 public:
-  KMeans(int D, int K) : D(D), K(K), C(K, D), v(K), initialized(false)
-  {
-  }
+  /**
+   * Create KMeans object.
+   * @param D number of features
+   * @param K number of centers
+   */
+  KMeans(int D, int K);
 
   /**
    * Sequential update.
    * @param X new training data, note that the number of samples per update
    *          must never change!
    */
-  void update(const Eigen::MatrixXd& X)
-  {
-    OPENANN_CHECK_EQUALS(X.cols(), D);
-
-    if(!initialized)
-      initialize(X);
-
-    OPENANN_CHECK_EQUALS(X.rows(), clusterIndices.size());
-
-    findClusters(X);
-    updateCenters(X);
-  }
+  void update(const Eigen::MatrixXd& X);
 
   /**
    * Compute for each instance the distances to the centers.
    * @param X each row represents an instance
    * @return each row contains the distances to all centers
    */
-  Eigen::MatrixXd operator()(const Eigen::MatrixXd& X)
-  {
-    const int N = X.rows();
-    Eigen::MatrixXd Y(N, K);
-    for(int n = 0; n < N; ++n)
-      for(int k = 0; k < K; k++)
-        Y(n, k) = (X.row(n) - C.row(k)).norm();
-    return Y;
-  }
+  Eigen::MatrixXd operator()(const Eigen::MatrixXd& X);
 
-  Eigen::MatrixXd getCenters()
-  {
-    return C;
-  }
+  /**
+   * Get the learned centers.
+   * @return each row represents a center
+   */
+  Eigen::MatrixXd getCenters();
 
 private:
-  void initialize(const Eigen::MatrixXd& X)
-  {
-    rng.generateIndices(X.rows(), clusterIndices, false);
-    for(int k = 0; k < K; ++k)
-      C.row(k) = X.row(clusterIndices[k]);
-    v.setZero();
-    initialized = true;
-  }
-
-  void findClusters(const Eigen::MatrixXd& X)
-  {
-    const int N = X.rows();
-    for(int n = 0; n < N; ++n)
-    {
-      int cluster = 0;
-      double shortedDistance = std::numeric_limits<double>::max();
-      for(int k = 0; k < K; k++)
-      {
-        const double distance = (C.row(k) - X.row(n)).squaredNorm();
-        if(distance < shortedDistance)
-        {
-          cluster = k;
-          shortedDistance = distance;
-        }
-      }
-      clusterIndices[n] = cluster;
-    }
-  }
-
-  void updateCenters(const Eigen::MatrixXd& X)
-  {
-    const int N = X.rows();
-    for(int n = 0; n < N; ++n)
-    {
-      const int cluster = clusterIndices[n];
-      v(cluster)++;
-      const double eta = 1.0 / (double) v(cluster);
-      C.row(cluster) += eta * (X.row(n) - C.row(cluster));
-    }
-  }
+  void initialize(const Eigen::MatrixXd& X);
+  void findClusters(const Eigen::MatrixXd& X);
+  void updateCenters(const Eigen::MatrixXd& X);
 };
 
 } // namespace OpenANN
---
+++ src/KMeans.cpp
@@ -0,0 +1,81 @@
+#include <OpenANN/KMeans.h>
+#include <limits>
+
+namespace OpenANN
+{
+
+KMeans::KMeans(int D, int K)
+  : D(D), K(K), C(K, D), v(K), initialized(false)
+{
+}
+
+void KMeans::update(const Eigen::MatrixXd& X)
+{
+  OPENANN_CHECK_EQUALS(X.cols(), D);
+
+  if(!initialized)
+    initialize(X);
+
+  OPENANN_CHECK_EQUALS(X.rows(), clusterIndices.size());
+
+  findClusters(X);
+  updateCenters(X);
+}
+
+Eigen::MatrixXd KMeans::operator()(const Eigen::MatrixXd& X)
+{
+  const int N = X.rows();
+  Eigen::MatrixXd Y(N, K);
+  for(int n = 0; n < N; ++n)
+    for(int k = 0; k < K; k++)
+      Y(n, k) = (X.row(n) - C.row(k)).norm();
+  return Y;
+}
+
+Eigen::MatrixXd KMeans::getCenters()
+{
+  return C;
+}
+
+void KMeans::initialize(const Eigen::MatrixXd& X)
+{
+  rng.generateIndices(X.rows(), clusterIndices, false);
+  for(int k = 0; k < K; ++k)
+    C.row(k) = X.row(clusterIndices[k]);
+  v.setZero();
+  initialized = true;
+}
+
+void KMeans::findClusters(const Eigen::MatrixXd& X)
+{
+  const int N = X.rows();
+  for(int n = 0; n < N; ++n)
+  {
+    int cluster = 0;
+    double shortedDistance = std::numeric_limits<double>::max();
+    for(int k = 0; k < K; k++)
+    {
+      const double distance = (C.row(k) - X.row(n)).squaredNorm();
+      if(distance < shortedDistance)
+      {
+        cluster = k;
+        shortedDistance = distance;
+      }
+    }
+    clusterIndices[n] = cluster;
+  }
+}
+
+void KMeans::updateCenters(const Eigen::MatrixXd& X)
+{
+  const int N = X.rows();
+  for(int n = 0; n < N; ++n)
+  {
+    const int cluster = clusterIndices[n];
+    v(cluster)++;
+    const double eta = 1.0 / (double) v(cluster);
+    C.row(cluster) += eta * (X.row(n) - C.row(cluster));
+  }
+}
+
+} // namespace OpenANN
