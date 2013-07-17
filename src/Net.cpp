ce3916b7fa607581f089ea35aaf42963336a01ef
Alexander Fabisch <afabisch@googlemail.com>
2013-07-17 09:05:17 +0200

    Log architecture while loading.

--- src/Net.cpp
+++ src/Net.cpp
@@ -262,6 +262,8 @@ void Net::load(const std::string& fileName)
       double stdDev;
       bool backprop;
       file >> H >> cdN >> stdDev >> backprop;
+      OPENANN_DEBUG << "rbm " << H << " " << cdN << " " << stdDev << " "
+          << backprop;
       restrictedBoltzmannMachineLayer(H, cdN, stdDev, backprop);
     }
     else if(type == "compressed")
@@ -273,6 +275,8 @@ void Net::load(const std::string& fileName)
       double stdDev;
       bool bias;
       file >> units >> params >> act >> compression >> stdDev >> bias;
+      OPENANN_DEBUG << "compressed " << units << " " << params << " " << act
+          << " " << compression << " " << stdDev << " " << bias;
       compressedLayer(units, params, (ActivationFunction) act, compression,
                       stdDev, bias);
     }
@@ -283,6 +287,8 @@ void Net::load(const std::string& fileName)
       double stdDev;
       bool bias;
       file >> units >> act >> stdDev >> bias;
+      OPENANN_DEBUG << "extreme " << units << " " << act << " " << stdDev
+          << " " << bias;
       extremeLayer(units, (ActivationFunction) act, stdDev, bias);
     }
     else if(type == "intrinsic_plasticity")
@@ -290,6 +296,7 @@ void Net::load(const std::string& fileName)
       double targetMean;
       double stdDev;
       file >> targetMean >> stdDev;
+      OPENANN_DEBUG << "intrinsic_plasticity " << targetMean << " " << stdDev;
       intrinsicPlasticityLayer(targetMean, stdDev);
     }
     else if(type == "convolutional")
@@ -298,6 +305,8 @@ void Net::load(const std::string& fileName)
       double stdDev;
       bool bias;
       file >> featureMaps >> kernelRows >> kernelCols >> act >> stdDev >> bias;
+      OPENANN_DEBUG << "convolutional " << featureMaps << " " << kernelRows
+          << " " << kernelCols << " " << act << " " << stdDev << " " << bias;
       convolutionalLayer(featureMaps, kernelRows, kernelCols,
                          (ActivationFunction) act, stdDev, bias);
     }
@@ -307,6 +316,8 @@ void Net::load(const std::string& fileName)
       double stdDev;
       bool bias;
       file >> kernelRows >> kernelCols >> act >> stdDev >> bias;
+      OPENANN_DEBUG << "subsampling " << kernelRows << " " << kernelCols
+          << " " << act << " " << stdDev << " " << bias;
       subsamplingLayer(kernelRows, kernelCols, (ActivationFunction) act,
                        stdDev, bias);
     }
@@ -314,6 +325,7 @@ void Net::load(const std::string& fileName)
     {
       int kernelRows, kernelCols;
       file >> kernelRows >> kernelCols;
+      OPENANN_DEBUG << "max_pooling " << kernelRows << " " << kernelCols;
       maxPoolingLayer(kernelRows, kernelCols);
     }
     else if(type == "local_response_normalization")
@@ -321,12 +333,15 @@ void Net::load(const std::string& fileName)
       double k, alpha, beta;
       int n;
       file >> k >> n >> alpha >> beta;
+      OPENANN_DEBUG << "local_response_normalization " << k << " " << n << " "
+          << alpha << " " << beta;
       localReponseNormalizationLayer(k, n, alpha, beta);
     }
     else if(type == "dropout")
     {
       double dropoutProbability;
       file >> dropoutProbability;
+      OPENANN_DEBUG << "dropout " << dropoutProbability;
       dropoutLayer(dropoutProbability);
     }
     else if(type == "output")
@@ -349,6 +364,8 @@ void Net::load(const std::string& fileName)
       double stdDev;
       bool bias;
       file >> units >> params >> act >> compression >> stdDev >> bias;
+      OPENANN_DEBUG << "compressed_output " << units << " " << params << " "
+          << act << " " << compression << " " << stdDev << " " << bias;
       compressedOutputLayer(units, params, (ActivationFunction) act,
                             compression, stdDev, bias);
     }
@@ -356,12 +373,15 @@ void Net::load(const std::string& fileName)
     {
       int errorFunction;
       file >> errorFunction;
+      OPENANN_DEBUG << "error_function " << errorFunction;
       setErrorFunction((ErrorFunction) errorFunction);
     }
     else if(type == "regularization ")
     {
       double l1Penalty, l2Penalty, maxSquaredWeightNorm;
       file >> l1Penalty >> l2Penalty >> maxSquaredWeightNorm;
+      OPENANN_DEBUG << "regularization " << l1Penalty << " " << l2Penalty
+          << " " << maxSquaredWeightNorm;
       setRegularization(l1Penalty, l2Penalty, maxSquaredWeightNorm);
     }
     else if(type == "parameters")
