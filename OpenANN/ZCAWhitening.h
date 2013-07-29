43da37463fae31da4b9d77dbaf0ad5474993795d
Alexander Fabisch <afabisch@tzi.de>
2013-07-29 22:34:22 +0200

    Add python binding for ZCA.

--- OpenANN/ZCAWhitening.h
+++ OpenANN/ZCAWhitening.h
@@ -10,16 +10,18 @@ namespace OpenANN
 /**
  * @class ZCAWhitening
  *
- * Zero component analysis whitening transformation.
+ * Zero phase component analysis whitening transformation.
  *
  * The data will be transformed through \f$ Y = X W \f$ such that the
  * covariance matrix \f$ C = \frac{1}{n-1} Y^T Y \f$ will be \f$ I \f$ and
- * \f$ W = W^T \f$.
+ * \f$ W = W^T \f$. This is essentially a PCA and a transformation back to
+ * the original space.
  */
 class ZCAWhitening : public Transformer
 {
   Eigen::MatrixXd W;
 public:
+  ZCAWhitening() {}
   virtual Transformer& fit(const Eigen::MatrixXd& X);
   virtual Eigen::MatrixXd transform(const Eigen::MatrixXd& X);
 };
