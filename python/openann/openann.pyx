933a33c2dadc9b28b0319a4efee574d52f8ba56b
Alexander Fabisch <afabisch@googlemail.com>
2013-08-13 14:42:22 +0200

    Rename files for python bindings and dependencies.

--- python/openann/openann.pyx
+++ python/openann/openann.pyx
@@ -6,10 +6,10 @@ from cython.operator cimport dereference as deref
 from libcpp.string cimport string
 import warnings
 
-include "util.pyx"
-include "dataset.pyx"
-include "preprocessing.pyx"
-include "layer.pyx"
-include "net.pyx"
-include "optimization.pyx"
-include "evaluation.pyx"
+include "util.pxi"
+include "dataset.pxi"
+include "preprocessing.pxi"
+include "layer.pxi"
+include "net.pxi"
+include "optimization.pxi"
+include "evaluation.pxi"
