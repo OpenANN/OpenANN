1cb02a62da1178994b6031994017b3e423565d31
Alexander Fabisch <afabisch@googlemail.com>
2013-10-03 22:03:10 +0200

    Add weighted data set.

---
+++ OpenANN/io/WeightedDataSet.h
@@ -0,0 +1,45 @@
+#ifndef OPENANN_WEIGHTED_DATA_SET_H_
+#define OPENANN_WEIGHTED_DATA_SET_H_
+
+#include <OpenANN/io/DataSet.h>
+#include <vector>
+
+namespace OpenANN
+{
+
+/**
+ * @class WeightedDataSet
+ *
+ * Resampled dataset based on the original dataset.
+ *
+ * The probability of each instance to occur in the dataset is defined by the
+ * given weights. Note that the weights must sum up to one.
+ */
+class WeightedDataSet : public DataSet
+{
+  DataSet& dataSet;
+  std::vector<double> weights;
+  bool deterministic;
+  std::vector<int> originalIndices;
+public:
+  /**
+   * @param dataSet original dataset
+   * @param weights weights for each instance, must sum up to one
+   * @param deterministic use deterministic (roulette wheel) sampling
+   */
+  WeightedDataSet(DataSet& dataSet, std::vector<double>& weights,
+                  bool deterministic);
+  WeightedDataSet& updateWeights(std::vector<double>& weights);
+  virtual int samples();
+  virtual int inputs();
+  virtual int outputs();
+  virtual Eigen::VectorXd& getInstance(int n);
+  virtual Eigen::VectorXd& getTarget(int n);
+  virtual void finishIteration(Learner& learner) {}
+private:
+  void resample();
+};
+
+} // namespace OpenANN
+
+#endif // OPENANN_WEIGHTED_DATA_SET_H_
