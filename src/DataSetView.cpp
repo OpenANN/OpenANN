0b91e9904d7dd15a000e9eb2810e10b6d8f5a709
Willem Duminy <willem@bulksms.com>
2019-12-17 06:55:39 +0200

    Formatting

--- src/DataSetView.cpp
+++ src/DataSetView.cpp
@@ -58,7 +58,7 @@ DataSetView& DataSetView::shuffle()
 #if __cplusplus < 201300L		
   std::random_shuffle(indices.begin(), indices.end());
 #else
-  std::shuffle(indices.begin(), indices.end(),randomGenerator);
+  std::shuffle(indices.begin(), indices.end(), randomGenerator);
 #endif
   return *this;
 }
@@ -82,7 +82,7 @@ void split(std::vector<DataSetView>& groups, DataSet& dataset,
 		#if __cplusplus < 201300L		
 			std::random_shuffle(indices.begin(), indices.end());
 		#else
-			std::shuffle(indices.begin(), indices.end(),randomGenerator);
+			std::shuffle(indices.begin(), indices.end(), randomGenerator);
 		#endif
 	}
   for(int i = 0; i < numberOfGroups; ++i)
@@ -115,7 +115,7 @@ void split(std::vector<DataSetView>& groups, DataSet& dataset, double ratio,
 		#if __cplusplus < 201300L		
 			std::random_shuffle(indices.begin(), indices.end());
 		#else
-			std::shuffle(indices.begin(), indices.end(),randomGenerator);
+			std::shuffle(indices.begin(), indices.end(), randomGenerator);
 		#endif	
 	}
   groups.push_back(DataSetView(dataset, indices.begin(), indices.begin() + samples));
